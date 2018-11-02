4b0e5e1628f5870dcd0ac41e7275d6b538d0aaad
Gustave L Rosanio <gusrosanio@yahoo.com>
2018-11-02 16:40:33 +0000

    changed some css and added the tiwtter verification stuff to the contoller

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,7 +1,12 @@
 class ClientController < ApplicationController
     
     def home
-        
+        require 'twitter'
+        client = Twitter::REST::Client.new do |config|
+            config.consumer_key = "dJaGLikNUVQN8f8JMKs9rqY9k"
+            config.consumer_secret = "sGNrb28QTMe41K1lfDgYYeUKFVtJ6MCpj4mCWo4m1Zcc7vc7LD"
+            config.access_token = "866054735882842112-eV3q0dG3AHGqxaQHbXAZDfndVf8qwkB"
+            config.access_token_secret = "rAS2pKxnv9gyN98X2JgOw74mwEaURhIEx6Diz4cZqc6wX"
+      end
     end
-    
 end
