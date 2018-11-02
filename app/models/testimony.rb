03b3d17a4b85521a7bc23d9faecd9d0c6d2a453f
Gustave L Rosanio <gusrosanio@yahoo.com>
2018-11-02 17:17:26 +0000

    updated gems and tweaked the testimony.rb file

--- app/models/testimony.rb
+++ app/models/testimony.rb
@@ -2,7 +2,7 @@ class Testimony < ActiveRecord::Base
     attr_accessor :content, :screen_name, :tweet_id
     
     def self.pull_tweets
-        Twitter.favorites('GusBus64').each do |tweet|
+        Twitter.favorites('GusBus64', count: 10).each do |tweet|
             unless exists?(tweet_id: tweet.id)
             create!(
                 tweet_id: tweet.id,
