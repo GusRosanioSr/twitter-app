587310d8cba9612e7ee06491466cfc1f28be8112
Gustave L Rosanio <gusrosanio@yahoo.com>
2018-11-07 14:52:11 +0000

    changed the schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181102165030) do
+
+  create_table "testimonies", force: :cascade do |t|
+    t.string   "tweet_id"
+    t.string   "screen_name"
+    t.text     "content"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
